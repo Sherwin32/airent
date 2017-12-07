fbe7e67cc7bdaf77611d88680ad6397cc391a2d6
Sherwin32 <sherwin.shann@gmail.com>
2017-12-07 11:36:19 -0800

    Created Item model and now can render items in show page. Preparing to switch branch for some image uploading experiment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   include UsersHelper
   include GroupsHelper
+  include ItemsHelper
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController
 	def create
-		puts "create Item called"
+		new_item = Item.new(item_params)
+		new_item.owner_id = current_user.id
+		new_item.history_log = "#{Time.new}: Post created! \n"
+		target_group_id = params[:item][:group_target]
+		target_group = Group.find(target_group_id)
+		if target_group.items << new_item
+			flash[:success] = "Created new post in #{target_group.name}!"
+			redirect_to  user_path(current_user.id)
+		else
+			flash[:error] = "Oops! Something went wrong."
+			redirect_to  user_path(current_user.id)
+		end
 	end
+
+	private
+  def item_params
+    params.require(:item).permit(:title, :description, :price)
+  end
 end

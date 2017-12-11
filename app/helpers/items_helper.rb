f467e1ae1df18255b6c82153db66795fa21a7bb8
Sherwin32 <sherwin.shann@gmail.com>
2017-12-10 17:14:01 -0800

    users can now like or unlike posts

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+	def item_owner id
+		User.find(id)
+	end
+
+	def is_liked id
+		if current_user.liked_item_ids.include?(id)
+			true
+		else
+			false
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208184749) do
+ActiveRecord::Schema.define(version: 20171209002953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171208184749) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "liked_item_ids", default: [], array: true
   end
 
 end

252e442126ec0155df41bbd6169719b2725d8353
Sherwin32 <sherwin.shann@gmail.com>
2017-12-12 12:24:25 -0800

    try to fix the image issue on heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
 	# validates :post_image, :presence => true
 	belongs_to :group
-	attachment :post_image
+	attachment :post_image, type: :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :user_group_connectors, dependent: :destroy
   has_many :groups, through: :user_group_connectors
   has_many :items, through: :groups
-  # has_many :document_librarys, dependent: :destroy
-  # has_many :documents, through: :document_librarys
 
   def self.confirm(params)
     @user = User.find_by({:user_name => params[:user_name]})
